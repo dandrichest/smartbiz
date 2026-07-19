ff86b8ef6700de08a53e6d401c7db459137de1d3
DANIEL <dandrichest@gmail.com>
2026-07-19 17:56:35 +0100

    Frontend Sales Recording Added (#14)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
-import LoginForm from "./components/LoginForm"
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
+import SalesRecord from "./components/SalesRecord";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (userData) => {
+    setUser(userData);
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
 
   return (
     <>
-    <LoginForm />
+      {user ? (
+        <SalesRecord onLogout={handleLogout} />
+      ) : (
+        <LoginForm onLogin={handleLogin} />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
