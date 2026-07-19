bb3e46378ed175f93b5b0da9f65be13670304d51
DANIEL <dandrichest@gmail.com>
2026-07-19 17:16:26 +0100

    Sales History (#15)

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import connectDB from './DB/connection.js';
 import authRoutes from './src/routes/auth.js';
 import productRoutes from './src/routes/product.js';
+import salesRoutes from './src/routes/sales.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,6 +20,7 @@ app.use(cookieParser());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/sales', salesRoutes);
 
 // Connect to DB and then start the server
 connectDB().then(() => {
